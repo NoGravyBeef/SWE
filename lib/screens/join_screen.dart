88e36cfd536b060ec2a1a860dc50efebda2d2bec
NoGravyBeef <dyfldrpf9@naver.com>
2023-11-25 01:07:29 +0900

    회원가입화면 싺다 갈아엎음

--- lib/screens/join_screen.dart
+++ lib/screens/join_screen.dart
@@ -48,300 +48,218 @@ class _JoinScreenState extends State<JoinScreen> {
                 ),
               ),
               // 아이디 입력 필드
-              Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
-                  ),
-                  Flexible(
-                    flex: 11,
-                    child: Container(
-                      decoration: const ShapeDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 196, 196, 196),
-                            Color.fromARGB(255, 255, 255, 255)
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0.0, 0.12],
-                          tileMode: TileMode.clamp,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(18.0)),
-                        ),
-                      ),
-                      child: const JoinTextField(
-                        labelText: '아이디',
-                        hintText: 'Enter the ID',
-                      ),
+              Center(
+                child: Container(
+                  width: screenSize.width * 0.85,
+                  decoration: const ShapeDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 196, 196, 196),
+                        Color.fromARGB(255, 255, 255, 255)
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.12],
+                      tileMode: TileMode.clamp,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(18.0)),
                     ),
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
+                  child: const JoinTextField(
+                    labelText: '아이디',
+                    hintText: 'Enter the ID',
                   ),
-                ],
+                ),
               ),
               SizedBox(height: screenSize.height * 0.04),
               // 비밀번호 입력 필드
-              Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
-                  ),
-                  Flexible(
-                    flex: 11,
-                    child: Container(
-                      decoration: const ShapeDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 196, 196, 196),
-                            Color.fromARGB(255, 255, 255, 255)
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0.0, 0.12],
-                          tileMode: TileMode.clamp,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(18.0)),
-                        ),
-                      ),
-                      child: const JoinTextField(
-                        labelText: '비밀번호',
-                        hintText: 'Enter the Password',
-                      ),
+              Center(
+                child: Container(
+                  width: screenSize.width * 0.85,
+                  decoration: const ShapeDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 196, 196, 196),
+                        Color.fromARGB(255, 255, 255, 255)
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.12],
+                      tileMode: TileMode.clamp,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(18.0)),
                     ),
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
+                  child: const JoinTextField(
+                    labelText: '비밀번호',
+                    hintText: 'Enter the Password',
                   ),
-                ],
+                ),
               ),
               SizedBox(height: screenSize.height * 0.04),
               // 비밀번호 확인 필드
-              Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
-                  ),
-                  Flexible(
-                    flex: 11,
-                    child: Container(
-                      decoration: const ShapeDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 196, 196, 196),
-                            Color.fromARGB(255, 255, 255, 255)
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0.0, 0.12],
-                          tileMode: TileMode.clamp,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(18.0)),
-                        ),
-                      ),
-                      child: const JoinTextField(
-                        labelText: '비밀번호 확인',
-                        hintText: 'Check the Password',
-                      ),
+              Center(
+                child: Container(
+                  width: screenSize.width * 0.85,
+                  decoration: const ShapeDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 196, 196, 196),
+                        Color.fromARGB(255, 255, 255, 255)
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.12],
+                      tileMode: TileMode.clamp,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(18.0)),
                     ),
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
+                  child: const JoinTextField(
+                    labelText: '비밀번호 확인',
+                    hintText: 'Check the Password',
                   ),
-                ],
+                ),
               ),
               SizedBox(height: screenSize.height * 0.04),
               // 성 및 이름 입력 필드
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Flexible(
-                    flex: 4,
-                    child: SizedBox(width: screenSize.width * 0.05),
-                  ),
-                  Flexible(
-                    flex: 20,
-                    child: Container(
-                      decoration: const ShapeDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 196, 196, 196),
-                            Color.fromARGB(255, 255, 255, 255)
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0.0, 0.12],
-                          tileMode: TileMode.clamp,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(18.0)),
-                        ),
+                  Container(
+                    width: screenSize.width * 0.4,
+                    decoration: const ShapeDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 196, 196, 196),
+                          Color.fromARGB(255, 255, 255, 255)
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.0, 0.12],
+                        tileMode: TileMode.clamp,
                       ),
-                      child: const JoinTextField(
-                        labelText: '성',
-                        hintText: 'Last Name',
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(18.0)),
                       ),
                     ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.02),
-                  ),
-                  Flexible(
-                    flex: 20,
-                    child: Container(
-                      decoration: const ShapeDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 196, 196, 196),
-                            Color.fromARGB(255, 255, 255, 255)
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0.0, 0.12],
-                          tileMode: TileMode.clamp,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(18.0)),
-                        ),
-                      ),
-                      child: const JoinTextField(
-                        labelText: '이름',
-                        hintText: 'First Name',
-                      ),
+                    child: const JoinTextField(
+                      labelText: '성',
+                      hintText: 'Last Name',
                     ),
                   ),
-                  Flexible(
-                    flex: 4,
-                    child: SizedBox(width: screenSize.width * 0.05),
+                  SizedBox(
+                    width: screenSize.width * 0.05,
+                  ),
+                  Container(
+                    width: screenSize.width * 0.4,
+                    decoration: const ShapeDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 196, 196, 196),
+                          Color.fromARGB(255, 255, 255, 255)
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.0, 0.12],
+                        tileMode: TileMode.clamp,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(18.0)),
+                      ),
+                    ),
+                    child: const JoinTextField(
+                      labelText: '이름',
+                      hintText: 'First Name',
+                    ),
                   ),
                 ],
               ),
               SizedBox(height: screenSize.height * 0.06),
               // 회원가입 버튼
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Flexible(
-                    fit: FlexFit.tight,
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
-                  ),
-                  Flexible(
-                    flex: 10,
-                    fit: FlexFit.tight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 143, 204, 138),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.6),
-                            spreadRadius: -5.0,
-                            blurRadius: 10.0,
-                            offset: const Offset(0, 6.0),
+                  SizedBox(
+                    width: screenSize.width * 0.8,
+                    height: screenSize.height * 0.065,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // 취소 로직
+                      },
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.zero),
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xffa9d8ab)),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
                           ),
-                        ],
+                        ),
                       ),
-                      child: ClipRect(
-                        child: TextButton(
-                          onPressed: pressed,
-                          style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                          child: const Text(
-                            '회원가입',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontFamily: 'JetBrain',
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white,
-                              letterSpacing: 1,
-                            ),
+                      child: const Center(
+                        child: Text(
+                          '회원가입',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: 'JetBrain',
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                            letterSpacing: 1,
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                    fit: FlexFit.tight,
-                    flex: 1,
-                    child: SizedBox(width: screenSize.width * 0.05),
                   ),
                 ],
               ),
-              SizedBox(height: screenSize.height * 0.2),
+              SizedBox(height: screenSize.height * 0.15),
               // 구글로 시작 버튼
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Flexible(
-                    flex: 2,
-                    child: SizedBox(width: screenSize.width * 0.05),
-                  ),
-                  Flexible(
-                    flex: 10,
-                    fit: FlexFit.tight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.5),
-                            spreadRadius: -4.0,
-                            blurRadius: 7.0,
-                            offset: const Offset(0, 6.0),
+                  SizedBox(
+                    width: screenSize.width * 0.7,
+                    height: screenSize.height * 0.055,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // 취소 로직
+                      },
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.zero),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/google_logo.png',
+                            width: screenSize.height * 0.05,
+                            height: screenSize.height * 0.05,
+                          ),
+                          SizedBox(width: screenSize.width * 0.03),
+                          const Text(
+                            'Start with google',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'JetBrain',
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromARGB(255, 116, 115, 115),
+                              letterSpacing: 1,
+                            ),
                           ),
                         ],
                       ),
-                      child: TextButton(
-                        onPressed: pressed,
-                        style: TextButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/google_logo.png',
-                              width: 30,
-                              height: 30,
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            const Text(
-                              'Start with google',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'JetBrain',
-                                fontWeight: FontWeight.w600,
-                                color: Color.fromARGB(255, 116, 115, 115),
-                                letterSpacing: 1,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
                     ),
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: SizedBox(width: screenSize.width * 0.05),
                   ),
                 ],
               ),
