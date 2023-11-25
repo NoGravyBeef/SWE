1b26d551c1eb1fbbf06281a6c6e85742e23d7b0c
stomachyoona <dbsdksp7797@naver.com>
2023-11-26 03:00:05 +0900

    추가함

---
+++ lib/widgets/Memomain.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Memomain extends StatefulWidget {
+  late final TextEditingController memoController;
+  @override
+  Memomain({required this.memoController});
+
+  _MemomainState createState() => _MemomainState();
+}
+
+class _MemomainState extends State<Memomain> {
+  final TextEditingController _memoController = TextEditingController();
+  bool _isMemoChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Flexible(
+      fit: FlexFit.tight,
+      flex: 5,
+      child: IntrinsicHeight(
+        child: Container(
+          decoration: ShapeDecoration(
+            color: Color(0xFFFFF3D9),
+            shadows: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, 3),
+                blurRadius: 1.0,
+                spreadRadius: 2.0,
+              ),
+            ],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(80.0),
+            ),
+          ),
+          child: TextFormField(
+            inputFormatters: [LengthLimitingTextInputFormatter(20)],
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.grey,
+            ),
+            controller: widget.memoController,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.04,
+              ),
+              errorStyle: TextStyle(
+                color: Colors.red,
+                fontSize: 10,
+                height: 0,
+              ),
+              errorMaxLines: 1,
+              border: InputBorder.none,
+            ),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            onSaved: (val) {},
+            validator: (val) {
+              if (val!.length >= 20) {
+                return 'error';
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
