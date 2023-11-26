e6678f63489c56d41abddd22b4ae2cc8b8734c71
stomachyoona <dbsdksp7797@naver.com>
2023-11-27 00:46:32 +0900

    오류팝업 해결했다~~!!!!!!!

--- lib/widgets/memo_text.dart
+++ lib/widgets/memo_text.dart
@@ -35,32 +35,46 @@ class _MemomainState extends State<Memomain> {
           ),
           //글자 작성
           child: TextFormField(
-            inputFormatters: [LengthLimitingTextInputFormatter(20)],
-            style: TextStyle(
-              fontSize: 15, //메모 글자 크기 변경
-              color: Colors.grey, //메모 글자 색 변경
-            ),
-            controller: widget.memoController,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.04,
+              inputFormatters: [LengthLimitingTextInputFormatter(20)],
+              style: TextStyle(
+                fontSize: 15, //메모 글자 크기 변경
+                color: Colors.grey, //메모 글자 색 변경
               ),
-              errorStyle: TextStyle(
-                color: Colors.red,
-                fontSize: 10,
-                height: 0,
+              controller: widget.memoController,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.04,
+                ),
+                border: InputBorder.none,
               ),
-              errorMaxLines: 1,
-              border: InputBorder.none,
-            ),
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            onSaved: (val) {},
-            validator: (val) {
-              if (val!.length >= 20) {
-                return 'error';
-              }
-            },
-          ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              onSaved: (val) {},
+              onChanged: (val) {
+                // 이 함수는 텍스트가 변경될 때 호출됩니다.
+                if (val.length >= 20) {
+                  // 텍스트 길이가 20자를 초과하면 다이얼로그를 표시합니다.
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        backgroundColor: Colors.white,
+                        title: const Text('Error'),
+                        content: const Text('텍스트 길이는 20자를 초과할 수 없습니다.'),
+                        actions: [
+                          TextButton(
+                            style:
+                                TextButton.styleFrom(primary: Colors.redAccent),
+                            onPressed: () {
+                              Navigator.pop(context); // 다이얼로그를 닫습니다.
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              }),
         ),
       ),
     );
